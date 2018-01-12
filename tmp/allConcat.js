dfb44dd23aa834ab7310c47fe29c32d37591b31d
Chris K Johnson <cknightjohnson@gmail.com>
2018-01-12 11:01:01 -0800

    add logic to concat query into url format

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,13 +1,14 @@
 import { apiCall } from './../js/doctor.js';
+import { searchQueryPrepper } from './../js/searchQueryPrepper.js';
 export function displayDocInfo(docInfo){
   $('h1').text(docInfo);
 }
 $(document).ready(function(){
-  $('#user-form').submit(function(){
-    this.preventDefault();
+  $('#user-form').submit(function(event){
+    event.preventDefault();
     let userAilment = $('#user-aliment').val();
     let docName = $('#doc-name').val();
     let searchLocation = $('#search-location').val();
-    apiCall(displayDocInfo, userAilment, docName, searchLocation);
+    apiCall(displayDocInfo, searchQueryPrepper(userAilment, docName, searchLocation));
   });
 });
