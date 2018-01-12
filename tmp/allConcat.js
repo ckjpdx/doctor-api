72f1c09dcf571706f20555c033628d1054f4b063
Chris K Johnson <cknightjohnson@gmail.com>
2018-01-12 12:13:00 -0800

    lunch break

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,7 +1,18 @@
 import { apiCall } from './../js/doctor.js';
 import { searchQueryPrepper } from './../js/searchQueryPrepper.js';
 function displayDocInfo(docInfo){
-  $('h1').text(docInfo);
+  $('#doc-list').empty();
+  for (let i = 0; i < docInfo.data.length; i++) {
+    let vAddress = docInfo.data[i].practices[0].visit_address;
+    if (vAddress === undefined) {
+      vAddress = `No Address Found`;
+    }
+    $('#doc-list').append(`<ul>
+      <li>${docInfo.data[i].profile.first_name} ${docInfo.data[i].profile.last_name}</li>
+      <li>${vAddress.street}, <em>${vAddress.street2}</em>, ${vAddress.city} ${vAddress.zip}</li>
+      <li>${docInfo.data[i].practices[0].phones[0].number}</li>
+      <ul>`);
+  }
 }
 $(document).ready(function(){
   $('#user-form').submit(function(event){
